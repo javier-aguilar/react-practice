f50681c17f8801343c1b0882271841e83d1f8d63
Javier Aguilar <pippo@Javiers-MacBook-Air.local>
2023-06-28 14:08:44 -0600

    Filter stories by searchTerm

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,58 @@
 import * as React from 'react'
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const App = () => (
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
+  const searchedStories = stories.filter((story) => 
+     story.title.toLowerCase().includes(searchTerm.toLowerCase())
+     );
+
+  return (
   <div>
     <h1>My Hacker Stories</h1>
 
-    <Search />
+    <Search onSearch={handleSearch} />
 
     <hr />
 
-    <List />
+    <List list={searchedStories} />
+  </div>);
+};
+
+const Search = (props) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={props.onSearch} />
   </div>
 );
 
-const Search = () => {
-  const handleChange = (event) => {
-    console.log(event);
-    console.log(event.target.value)
-  }
-
-  return (  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" onChange={handleChange} />
-  </div>
-)
-};
-
-const List = () => (
+const List = (props) => (
   <ul>
-    {list.map((item) => (
+    {props.list.map((item) => (
       <li key={item.objectID}>
         <span>
           <a href={item.url}>{item.title}</a>
