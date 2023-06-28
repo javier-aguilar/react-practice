d92b194ab6a3d7912b86c4f173accd9f8c2b7793
Javier Aguilar <pippo@Javiers-MacBook-Air.local>
2023-06-28 14:11:54 -0600

    Add state value to Search component

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
   <div>
     <h1>My Hacker Stories</h1>
 
-    <Search onSearch={handleSearch} />
+    <Search search={searchTerm} onSearch={handleSearch} />
 
     <hr />
 
@@ -46,7 +46,7 @@ const App = () => {
 const Search = (props) => (
   <div>
     <label htmlFor="search">Search: </label>
-    <input id="search" type="text" onChange={props.onSearch} />
+    <input id="search" type="text" value={props.search} onChange={props.onSearch} />
   </div>
 );
 
